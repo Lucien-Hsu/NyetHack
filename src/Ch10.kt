6a30dcf3b541cdbff87ce7f7f046a15ecc20344a
Lucien-Hsu <meteor831@gmail.com>
2020-09-17 22:09:39 +0800

    Ch10 完成

--- src/Ch10.kt
+++ src/Ch10.kt
@@ -57,16 +57,69 @@ fun main(args: Array<String>) {
 //        println("第${index}個元素為：$word")
 //    }
 //
-    //10.3
-    //讀取檔案內容，分割後丟給List
-    //在此的檔案路徑為data/menu.txt
-    val menuList = File("data/menu.txt")
-        .readText()
-        .split("\n")
-    //印出
-    menuList.forEachIndexed{index, content ->
-        print("[$index]：$content\n")
-    }
+//    //10.3
+//    //讀取檔案內容，分割後丟給List
+//    //在此的檔案路徑為data/menu.txt
+//    val menuList = File("data/menu.txt")
+//        .readText()
+//        .split("\n")
+//    //印出
+//    menuList.forEachIndexed{index, content ->
+//        print("[$index]：$content\n")
+//    }
+//
+//    //10.4
+//    val testStr = "A,b,c,d,e,f,g,h"
+//    //最多可解構前五個元素
+//    //可用底線過濾掉不需解構的元素
+//    val (x1,x2,x3,_,x5) = testStr.split(",")
+//    print(x1 + " ")
+//    print(x2 + " ")
+//    print(x3 + " ")
+//    print(x5 + " ")
+//
+//    //10.5
+//    //宣告不可變集合
+//    val planets = setOf("Earth", "Jupiter", "Mars", "Earth")   //會自動移除重複值
+//    println(planets)
+//    //檢查是否包含元素
+//    println("包含Earth：" + planets.contains("Earth"))
+//    println("包含Venus：" + planets.contains("Venus"))
+//    //取指定元素
+//    println("第2個元素為：" + planets.elementAt(1))
+//    //宣告可變集合
+//    val fruits = mutableSetOf("kiwi", "mango", "apple")
+//    //增加元素
+//    fruits += listOf("banana", "apple")
+//    println(fruits)
+//
+//    //10.8
+//    val firstList = mutableListOf("A", "B", "C", "A")
+//    println(firstList)
+//    //轉為set再轉為list可消除重複元素。
+//    val secondList = firstList.toSet().toList()
+//    println(secondList)
+//    //使用distinct也可直接消除重複元素
+//    val thirdList = firstList.distinct()
+//    println(thirdList)
+//
+//    //10.9
+//    //建立Java中的int基礎類型的陣列
+//    val xx: IntArray = intArrayOf(1,2,3)
+//    //建立List，但在需要時用toIntArray()轉換為int基礎類型的陣列
+//    val yy: List<Int> = listOf(1,2,3)
+//    val zz: IntArray = yy.toIntArray()
 
+    //10.10
+    //可變的情況一
+    val o = listOf(mutableListOf(1,2,3))
+    val p = listOf(mutableListOf(1,2,3))
+    println(o == p)
+    o[0].add(4)
+    println(o == p)
+    //可變的情況二
+    var tmpList: List<Int> = listOf(1,2,3)
+    (tmpList as MutableList)[2] = 1000
+    println(tmpList)
 
 }
