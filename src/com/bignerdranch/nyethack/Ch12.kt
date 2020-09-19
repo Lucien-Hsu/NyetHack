3810b2513b0571da84beede25652f97961def8da
Lucien-Hsu <meteor831@gmail.com>
2020-09-19 21:24:21 +0800

    Ch12 完成

--- src/com/bignerdranch/nyethack/Ch12.kt
+++ src/com/bignerdranch/nyethack/Ch12.kt
@@ -1,1 +1,24 @@
 package com.bignerdranch.nyethack
+
+//12.9 競態條件
+class Weapon(val name: String)
+class Player2{
+    var weapon: Weapon? = Weapon("Sword")
+
+    fun printWeaponName(){
+        //此寫法會產生競態條件
+        //因為if判斷完後，在println之前，weapon有可能被其他程式改為null
+//        if(weapon != null){
+//            println(weapon.name)
+//        }
+
+        //此寫法相當於把判斷完非null的值傳給it，it是唯讀的，因此確保了值不更動
+        weapon?.also{
+            println(it.name)
+        }
+    }
+}
+
+fun main(args: Array<String>) {
+    Player2().printWeaponName()
+}
--- src/com/bignerdranch/nyethack/Player.kt
+++ src/com/bignerdranch/nyethack/Player.kt
@@ -47,3 +47,4 @@ class Player {
             else -> "is in awful condition!"
         }
 }
+
