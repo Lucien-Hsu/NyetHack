629c5802bfab862db7a8d20af6a6d135b7a2a37d
Lucien-Hsu <meteor831@gmail.com>
2020-09-23 19:01:41 +0800

    Ch15.5 完成

--- src/com/bignerdranch/nyethack/Navigation.kt
+++ src/com/bignerdranch/nyethack/Navigation.kt
@@ -10,14 +10,18 @@ enum class Direction(private val coordinate: Coordinate) {
 
     //列舉類別函數
     fun updateCoordinate(playerCoodinate: Coordinate) =
-        Coordinate(
-            playerCoodinate.x + coordinate.x,
-            playerCoodinate.y + coordinate.y
-        )
+        coordinate + playerCoodinate    //15.5 運算子重載，將列舉常數的座標加上玩家座標
+//        Coordinate(
+//            playerCoodinate.x + coordinate.x,
+//            playerCoodinate.y + coordinate.y
+//        )
 }
 //呼叫列舉常數的方法
 //Direction.EAST.updateCoordinate(Coordinate(1, 0))
 
 data class Coordinate(val x: Int, val y: Int) {
     val isInBounds = x >= 0 && y >= 0
+
+    //使用operator關鍵字讓運算子重載
+    operator fun plus(other: Coordinate) = Coordinate(x + other.x, y + other.y)
 }
