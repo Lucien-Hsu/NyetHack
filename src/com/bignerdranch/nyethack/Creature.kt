10212e1fd57cc16bd66f7aa11b1b41c8340463a7
Lucien-Hsu <meteor831@gmail.com>
2020-09-25 23:16:13 +0800

    Ch16.4 完成

--- src/com/bignerdranch/nyethack/Creature.kt
+++ src/com/bignerdranch/nyethack/Creature.kt
@@ -16,3 +16,30 @@ interface Fightable {
     //介面可作為參數類型
     fun attack(opponent: Fightable): Int
 }
+
+//16.4 抽象類別
+//怪物
+abstract class Monster(
+    val name: String,
+    val description: String,
+    override var healthPoints: Int
+) : Fightable {
+
+    override fun attack(opponent: Fightable): Int {
+        val damageDealt = damageRoll
+        opponent.healthPoints -= damageDealt
+        return damageDealt
+    }
+}
+
+//16.4
+//哥布林
+//抽象類別之子類別需實作所有介面的成員，可繼承也可自己實作
+class Goblin(
+    name: String = "Goblin",
+    description: String = "A nasty-looking goblin",
+    healthPoints: Int = 30
+) : Monster(name, description, healthPoints) {
+    override val diceCount: Int = 2
+    override val diceSides: Int = 8
+}
