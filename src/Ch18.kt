3210bd06b7f40f792ee2e5e9020ee0cc8dfe651f
Lucien-Hsu <meteor831@gmail.com>
2020-09-28 22:04:24 +0800

    Ch18.2 完成

--- src/Ch18.kt
+++ src/Ch18.kt
@@ -1,7 +1,12 @@
 //定義String的擴充函數
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 //定義超類別Any的擴充函數
-fun Any.easyPrint() = print(this)
+//fun Any.easyPrint() = print(this)
+//以泛型改寫為支援鍊式呼叫
+fun <T> T.easyPrint(): T{
+    println(this)
+    return this
+}
 
 fun main(args: Array<String>) {
     //使用String的擴充函數
@@ -10,4 +15,7 @@ fun main(args: Array<String>) {
     //使用超類別Any的擴充函數
     "Welcome~\n".easyPrint()
     886.easyPrint()
+    //使用支援鍊式呼叫的函數
+    "Hi~Hi~~~".easyPrint().addEnthusiasm(3).easyPrint()
+
 }
